40467702f054cf7afdd2281538cec34cc9001db9
gawel1337 <tomasz.gawel@lo1.sandomierz.pl>
2020-01-23 12:36:19 +0100

    kalkulator v2

--- python/kalkulator.py
+++ python/kalkulator.py
@@ -3,30 +3,50 @@
 #
 #  kalkulator.py
 
-def operacja():
-    operacja = str(input("Jakie działanie chcesz wykonac +, -, / , *")
+wynik = 0
+operacja = ' '
+ 
+def operacja( ):
+    operacja = input("Jakie działanie chcesz wykonac +, -, / , *: ")
+    return 0
     
-def lcb():
-    lcb1 = int(input("Podaj  2 liczbę:"))
+def lcb( lcb1, lcb2):
+    lcb1 = int(input("Podaj  liczbę:"))
     lcb2 = int(input("Podaj  2 liczbę:"))
-def dodaj():
-    suma = lcb1 + lcb2
+    return 0
     
-def odejmij():
-    roznica = lcb1 - lcb2 
-
-def pomnoz():
-    iloczyn =lcb1 * lcb2
+def dodaj(lcb1, lcb2):
+    global wynik
+    wynik = (lcb1 + lcb2)
+    return 0
     
-def podziel():
-    iloraz = lcb1/lcb2
-
-def drukujWynik()
-
+def odejmij(lcb1, lcb2):
+    global wynik  
+    wynik = (lcb1 - lcb2)
+    return 0
+     
+def pomnoz(lcb1, lcb2):
+    iloczyn = (lcb1  *  lcb2)
+    return 0
+    
+def podziel(lcb1 , lcb2):
+    iloraz = (lcb1 / lcb2)
+    return 0
+    
+def Wynik():
+    if operacja == '+':
+        dodaj(lcb1, lcb2)
+    elif operacja == '-':
+        odejmij(lcb1, lcb2)
+    elif operacja == '*':
+        pomnoz(lcb1, lcb2)
+    elif operacja =='/':
+        podziel(lcb1, lcb2)
+    
 def main(args):
     operacja()
     lcb()
-    wynik = drukujWynik()
+    Wynik()
     return 0
 
 if __name__ == '__main__':
