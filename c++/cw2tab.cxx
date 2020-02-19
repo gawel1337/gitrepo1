878769b632b7b3e7f979a6d187379f9e24de4ef5
gawel1337 <tomasz.gawel@lo1.sandomierz.pl>
2020-02-19 22:34:51 +0100

    tabela v3

--- c++/cw2tab.cxx
+++ c++/cw2tab.cxx
@@ -28,6 +28,17 @@ void sumujW(int t[][10], int w, int k, int n) {
         cout << setw(6) << sumaW << endl;
     }   
 }
+void sumujK (int t[][10], int w, int k, int n) {
+	int sumaK = 0;
+	for(int i=0; i<w; i++){
+		for(int j=0; j<k; j++) {
+            cout << setw(4) << t[i][j];
+            sumaW += t[i][j];
+        }
+        cout << setw(6) << sumaW << endl;
+    }   
+}
+	
 
 
 int main(int argc, char **argv) {
@@ -39,6 +50,7 @@ int main(int argc, char **argv) {
     int t[w][10] ; 
     wypelnij (t, w, k, n);
     sumujW (t, w, k, n);
+    sumujK (t, w, k, n);
     return 0;
 }
 
